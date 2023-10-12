937b3433fc63406679fd5f2b8086588b81a6712b
Taishi-N324 <taishiahiru5@gmail.com>
2023-10-12 10:42:54 +0200

    update

--- fmengine_multi_node.sh
+++ fmengine_multi_node.sh
@@ -1,9 +1,9 @@
 #!/bin/bash -x
 #SBATCH --account=cstdl
-#SBATCH --nodes=2
+#SBATCH --nodes=160
 #SBATCH --ntasks-per-node=1
 #SBATCH --gres=gpu:4
-#SBATCH --partition=develbooster
+#SBATCH --partition=booster
 #SBATCH --output=/p/home/jusers/nakamura2/juwels/nakamura2/fmengine/%j_%N_log.out  # change this line to your output file
 
 # Network Configuration
@@ -31,7 +31,7 @@ master_addr="$(scontrol show hostnames "$SLURM_JOB_NODELIST" | head -n 1)i"
 export MASTER_ADDR=$master_addr
 
 export HOSTNAMES=`scontrol show hostnames "$SLURM_JOB_NODELIST"`
-export MASTER_ADDR="$(scontrol show hostnames "$SLURM_JOB_NODELIST" | head -n 1)"
+# export MASTER_ADDR="$(scontrol show hostnames "$SLURM_JOB_NODELIST" | head -n 1)"
 export MASTER_PORT=12802
 export COUNT_NODE=`scontrol show hostnames "$SLURM_JOB_NODELIST" | wc -l`
 
@@ -60,15 +60,15 @@ do
     --nproc-per-node=$NUM_GPUS \
         cli/train.py \
         --output_dir /workspace/.cache/models \
-        --init_ckpt /pretrained/llama-2-7b-hf \
+        --init_ckpt /pretrained/llama-2-70b-hf-4shard \
         --data_path /datasets/prompt.jsonl \
-        --max_seq_len 2048 \
+        --max_seq_len 4096 \
         --train_steps 10 \
-        --eval_steps 10 \
+        --eval_steps 50 \
         --save_steps 1000 \
         --log_steps 1 \
-        --pipe_parallel_size 8 \
-        --model_parallel_size 1 \
+        --pipe_parallel_size 1 \
+        --model_parallel_size 4 \
         --use_flash_attn true \
         --use_fused_ops true \
         --deepspeed_config ./configs/llama.json &
